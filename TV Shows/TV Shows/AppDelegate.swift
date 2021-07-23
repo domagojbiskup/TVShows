c74b087a592041712b9575e97ed3d1502e14d141
Domagoj Biskup <domagoj.biskup@live.com>
2021-07-23 22:58:07 +0200

    Implemented secure text entry in the password text field. Improved remember me programming logic (taken from a review from homework-02). And some elements are renamed to be more descriptive. With this now I'm done and ready for a review.

--- TV Shows/TV Shows/AppDelegate.swift
+++ TV Shows/TV Shows/AppDelegate.swift
@@ -16,45 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-/*
-        let params: [String: String] =  [
-            "email": "dbiskup@foi.hr",
-            "password": "",
-            "password_conformation": ""
-        ]
-
-
-        AF
-            .request(
-                "https://tv-shows.infinum.academy/users",
-//                "https://tv-shows.infinum.academy/users/sign_in",
-                method: .post,
-                parameters: params,
-                encoder: JSONParameterEncoder.default
-            )
-            .validate()
-            .responseDecodable(of: UserResponse.self) { response in
-                switch response.result {
-                case .success(let userResponse):
-                    print("email: \(userResponse.user.mail)")
-                    print("id: \(userResponse.user.id)")
-                    print("image url: \(userResponse.user.imageUrl)")
-                case .failure(let error):
-                    print("error: \(error)")
-                }
-            }
-
-/*
-            .responseJSON(completionHandler: { response in
-                switch response.result {
-                case .success (let body):
-                    print("Success: \(body)")
-                case .failure(let error):
-                    print("Error: \(error)")
-                }
-            })
-*/
-*/
         return true
     }
 }
--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -10,51 +10,63 @@ import SVProgressHUD
 import Alamofire
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
-
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var passwordEye: UIButton!
-    @IBOutlet weak var checkBox: UIButton!
-    @IBOutlet weak var loginButton: UIButton!
-
-    private var loginButtonPressed = true
-    private var mail = ""
-    private var pass = ""
+    
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var passwordEyeButton: UIButton!
+    @IBOutlet private weak var checkBoxRememberMeButton: UIButton!
+    @IBOutlet private weak var loginButton: UIButton!
+    
+    private var email = ""
+    private var password = ""
+    private var passwordEyeActive = false
     private var checkedRememberMe = false
+    private var loginButtonPressed = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         emailTextField.delegate = self
         passwordTextField.delegate = self
-
+        
         loginButton.layer.cornerRadius = 17
         loginButton.layer.masksToBounds = true
     }
-
-    @IBAction func rememberMeButton(_ sender: UIButton) {
-
-        if !checkedRememberMe {
-            checkBox.setImage(UIImage(named: "ic-checkbox-selected"), for: .normal)
-            checkedRememberMe = true
+    
+    @IBAction func passwordEyeButton(_ sender: UIButton) {
+        
+        if passwordEyeActive {
+            passwordEyeButton.setImage(UIImage(named: "ic-visible"), for: .normal)
+            passwordTextField.isSecureTextEntry = true
         } else {
-            checkBox.setImage(UIImage(named: "ic-checkbox-unselected"), for: .normal)
-            checkedRememberMe = false
+            passwordEyeButton.setImage(UIImage(named: "ic-invisible"), for: .normal)
+            passwordTextField.isSecureTextEntry = false
         }
+        passwordEyeActive.toggle()
     }
-
+    
+    @IBAction func rememberMeButton(_ sender: UIButton) {
+        
+        if checkedRememberMe {
+            checkBoxRememberMeButton.setImage(UIImage(named: "ic-checkbox-unselected"), for: .normal)
+        } else {
+            checkBoxRememberMeButton.setImage(UIImage(named: "ic-checkbox-selected"), for: .normal)
+        }
+        checkedRememberMe.toggle()
+    }
+    
     func initializing() {
-        mail = emailTextField.text ?? ""
-        pass = passwordTextField.text ?? ""
+        email = emailTextField.text ?? ""
+        password = passwordTextField.text ?? ""
     }
-
+    
     func endTextFieldEditing() {
         emailTextField.endEditing(true)
         passwordTextField.endEditing(true)
     }
-
+    
     func textFieldChackerAndNetworking() {
-
+        
         /// Email checker
         if emailTextField.text == "" {
             emailTextField.placeholder = "This field cannot be empty!"
@@ -62,7 +74,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
                 self.emailTextField.placeholder = "Email"
             }
         }
-
+        
         /// Password checker
         if passwordTextField.text == "" {
             passwordTextField.placeholder = "This field cannot be empty!"
@@ -70,20 +82,20 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
                 self.passwordTextField.placeholder = "Password"
             }
         }
-
+        
         /// Networking
         if emailTextField.text != "" && passwordTextField.text != "" {
-            networking(email: mail, password: pass)
+            networking(email: email, password: password)
         }
     }
-
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         endTextFieldEditing()
         return true
     }
-
+    
     @IBAction func buttonPressed(_ sender: UIButton) {
-
+        
         if sender.titleLabel?.text == "Login" {
             loginButtonPressed = true
         } else if sender.titleLabel?.text == "Register" {
@@ -91,7 +103,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         } else {
             print("loginButtonPressed errored out")
         }
-
+        
         endTextFieldEditing()
         initializing()
         textFieldChackerAndNetworking()
@@ -99,16 +111,16 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
 }
 
 private extension LoginViewController {
-
+    
     func networking(email: String, password: String) {
-
+        
         SVProgressHUD.show()
-
+        
         let params: [String: String] = [
-            "email": mail,
-            "password": pass,
+            "email": email,
+            "password": password,
         ]
-
+        
         AF
             .request(
                 url(loginButtonPressed),
@@ -128,12 +140,12 @@ private extension LoginViewController {
                 }
             }
     }
-
+    
     func url(_ loginButtonPressed: Bool) -> String {
-
+        
         let loginUrl = "https://tv-shows.infinum.academy/users/sign_in"
         let regUrl = "https://tv-shows.infinum.academy/users"
-
+        
         if loginButtonPressed {
             return loginUrl
         } else if !loginButtonPressed {
@@ -143,3 +155,5 @@ private extension LoginViewController {
         }
     }
 }
+
+
